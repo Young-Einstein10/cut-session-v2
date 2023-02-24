213971eb0b8627579b040dc921b7007fc6dd3cfa
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2023-02-24 17:07:50 +0100

    chore: fix linting issues

--- theme/index.ts
+++ theme/index.ts
@@ -1,9 +1,8 @@
 // 1. Import the extendTheme function
-import { extendTheme, theme as chakraTheme } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import styles from "./global";
 import components from "./components";
-import colors from "./colors";
 
-const theme = extendTheme({ colors: colors(chakraTheme), styles, components });
+const theme = extendTheme({ styles, components });
 
 export default theme;
